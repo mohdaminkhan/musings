ae96863c2d88ac90adf95d198842dd61b151d151
mohdaminkhan <mohdaminkhan@hotmail.com>
2022-07-31 02:40:19 -0700

    added link and about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+
+function About() {
+
+
+	return <h1>This is about me</h1>
+}
+
+
+
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
-
+import Link from 'next/link'
 import { projects } from '../utils/projectsData';
 
 const Home = () => {
 
 
-	return <div className="home">Hello World!!</div>
+	return (<div className="home">
+
+		Hello World!!
+
+		<Link href="about"> <a>About</a> </Link>
+
+
+		</div>
+	)
 };
+
 
 export default Home;
