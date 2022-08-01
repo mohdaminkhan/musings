be6b1f06863688c3e896f8c05d3f6ac9de3f898e
mohdaminkhan <mohdaminkhan@hotmail.com>
2022-08-01 02:29:50 -0700

    changed the index and moduleResolution to Node

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,39 @@ import { projectIcons } from '../components/Icons';
 import Link from 'next/link'
 import { projects } from '../utils/projectsData';
 
+
+function Title() {
+
+
+
+
+	return (<div>
+
+
+	  <h1>THOUGHT FROM WITHIN THE FRONTEND TRENCHES</h1>	
+
+
+
+	</div>)
+
+
+}
+
+
+
+
+
 const Home = () => {
 
 
 	return (<div className="home">
 
 		Hello World!!
-
+		<Title />
 		<Link href="/about"><a>About</a></Link>
 
 
-		</div>
+	</div>
 	)
 };
 
