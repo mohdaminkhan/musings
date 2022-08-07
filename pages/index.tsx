f88bd72b3f5e38d188204a4eb9bef173091ed989
mohdaminkhan <mohdaminkhan@hotmail.com>
2022-08-07 11:41:36 -0700

    testing mutability of ref object

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,28 @@ import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 import Link from 'next/link'
 import { projects } from '../utils/projectsData';
+import * as React from 'react'
+
+
+
+function TestRef() {
+
+
+let myRef = React.useRef({name:"amin"})
+// how to read environment at runtime
+//  npm view <react> version <--- useful from the command line 
+
+
+console.log("Is Ref sealed", Object.isSealed(myRef))
+console.log("Is Ref extendible", Object.isExtensible(myRef))
+
+
+
+return <h1>THis is the Ref Component</h1>
+
+}
+
+
 
 
 function Title() {
@@ -12,9 +34,9 @@ function Title() {
 	return (<div>
 
 
-	  <h1>THOUGHT FROM WITHIN THE FRONTEND TRENCHES</h1>	
+		<h1>THOUGHT FROM WITHIN THE FRONTEND TRENCHES</h1>
 
-
+<TestRef />
 
 	</div>)
 
