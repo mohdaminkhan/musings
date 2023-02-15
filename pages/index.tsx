594a39ace50c1e2487956ec98c5ac3f8e8308ac8
amin khan <mohdaminkhan@hotmail.com>
2023-02-15 11:23:23 -0800

    some  clean up, also add export statement to links.tsx to allow tsc to treat it a a module(--isolatedModules/)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,29 @@
 import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
-import Link from 'next/link'
+import Link from 'next/link';
 import { projects } from '../utils/projectsData';
-import * as React from 'react'
-
-
-
-
-
-
-
+import * as React from 'react';
+import styles from '../styles/modules/global.module.scss';
 
 function Title() {
-
-
-
-
-	return (<div>
-
-
-		<h2>Musings of a Dev</h2>
-		<h1>MISSIVES FROM THE FRONTEND TRENCHES</h1>
-
-	</div>)
-
-
+  return (
+    <div>
+      <h2>Musings of a Dev</h2>
+      <h1 className={styles.testclass}>MISSIVES FROM THE FRONTEND TRENCHES</h1>
+    </div>
+  );
 }
 
-
-
-
-
 const Home = () => {
-
-
-	return (<div className="home">
-
-		Hello World!!
-		<Title />
-		<Link href="/about"><a>About</a></Link>
-
-
-	</div>
-	)
+  return (
+    <div className="home">
+      Hello World!!
+      <Title />
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
 };
-
 
 export default Home;
